bdbc01e1ff62445465442115c624b5b947ee56c2
jstringer <jstringer@imagenet.com>
2024-03-26 14:12:59 -0500

    Initial pub

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = `imagenet-chat-loader`;
+const appShellFiles = [
+    '/',
+    '/ChatpageJS.html',
+    '/manifest.json',
+    '/sw.js',
+    '/main.js',
+	'icon512.png'
+    // Include other assets like scripts, stylesheets, and images
+];
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(appShellFiles);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
