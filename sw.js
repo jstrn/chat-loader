db092bb5956591137e64433ac69d3f9e6e4d173e
jstrn <146207588+jstrn@users.noreply.github.com>
2024-03-29 13:11:30 -0500

    rollback sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,49 @@
-const CACHE = "chat-loader-offline";
+const CACHE_NAME = `imagenet-chat-loader`;
+const appShellFiles = [
+    '/',
+    '/index.html',
+    '/sw.js',
+    '/main.js',
+    '/android-chrome-192x192.png',
+    '/android-chrome-512x512.png',
+    '/apple-touch-icon.png',
+    '/browserconfig.xml',
+    '/favicon-16x16.png',
+    '/favicon-32x32.png',
+    '/favicon.ico',
+    '/mstile-150x150.png',
+    '/safari-pinned-tab.svg',
+    '/site.webmanifest'
+    // Include other assets like scripts, stylesheets, and images
+];
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(appShellFiles);
+  })());
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
