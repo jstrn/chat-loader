01637b9700d0dc6da136b43301ba95cc1edd78bd
jstrn <146207588+jstrn@users.noreply.github.com>
2024-03-29 14:10:46 -0500

    html

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const isDaytime = hour >= 7 && hour < 18; // 7:00 AM to 5:59 PM
     const container = document.getElementById('app-container');
 
-    let iframeHTML = '<iframe class="responsive-iframe" src="';
+    let iframeHTML = '<iframe class="responsive-iframe" title="app-iframe" src="';
     iframeHTML += isWeekday && isDaytime 
         ? 'https://messenger.chatgenie.io/?appId=992ebe24-9d7b-4b00-adc4-3e3faf72c434"'
         : 'https://mentorappweb.globalmentoring.com/client?clientId=MjQ5NTE4&clientType=TXNw&lang=ZW4%3D"';
