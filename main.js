4a609bcd87386a6ead7a0400f1d69c8b6e676eee
jstrn <146207588+jstrn@users.noreply.github.com>
2024-03-29 14:19:55 -0500

    add install prompt

--- main.js
+++ main.js
@@ -14,3 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
     container.innerHTML = iframeHTML;
 })
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+// main.js
+
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) {
+      return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
+
+window.addEventListener("appinstalled", () => {
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
